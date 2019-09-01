ec615700dd910dd844933e8b32f095608c3b56bb
Christoph Müller <cmller49@gmail.com>
2019-09-01 21:35:59 +0200

    Initial Commit   - Added Projects for:      - CLI      - GUI      - Library   - and added LCU Sharp by Bryan Hitchcock (https://github.com/BryanHitchcock/lcu-sharp)

---
+++ CLI/Program.cs
@@ -0,0 +1,13 @@
+﻿using Library;
+using System;
+
+namespace CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LeagueEnhancer leagueEnhancer = new LeagueEnhancer();
+        }
+    }
+}
---
+++ Library/LeagueEnhancer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using LCUSharp;
+using LCUSharp.Websocket;
+
+namespace Library
+{
+    public class LeagueEnhancer
+    {
+        public LeagueEnhancer()
+        {
+            Initialize();
+
+            Console.ReadKey();
+        }
+
+        public async void Initialize()
+        {
+            // Request
+            //ChangeSummonerIcon(58);
+            await EventExampleAsync();
+        }
+
+        public async void ChangeSummonerIcon(uint profileIconId)
+        {
+            // Initialize a connection to the league client.
+            var api = await LeagueClientApi.ConnectAsync();
+
+            // Update the current summoner's profile icon
+            var body = new { profileIconId = profileIconId };
+            var queryParameters = Enumerable.Empty<string>();
+            var json = await api.RequestHandler.GetJsonResponseAsync(HttpMethod.Put, "lol-summoner/v1/current-summoner/icon",
+                                                                     queryParameters, body);
+        }
+
+
+
+
+
+
+        public event EventHandler<LeagueEvent> GameFlowChanged;
+
+        public async Task EventExampleAsync()
+        {
+            // Initialize a connection to the league client.
+            var api = await LeagueClientApi.ConnectAsync();
+            Console.WriteLine("Connected!");
+
+            // Register game flow event.
+            GameFlowChanged += OnGameFlowChanged;
+            api.EventHandler.Subscribe("/lol-gameflow/v1/gameflow-phase", GameFlowChanged);
+
+            // Wait until work is complete.
+            Console.WriteLine("Done.");
+        }
+
+        private void OnGameFlowChanged(object sender, LeagueEvent e)
+        {
+            var result = e.Data.ToString();
+            var state = string.Empty;
+
+            if (result == "None")
+                state = "main menu";
+            else if (result == "ChampSelect")
+                state = "champ select";
+            else if (result == "Lobby")
+                state = "lobby";
+            else if (result == "InProgress")
+                state = "game";
+
+            // Print new state and set work to complete.
+            Console.WriteLine($"Status update: Entered {state}. ({result})");
+        }
+    }
+
+
+}

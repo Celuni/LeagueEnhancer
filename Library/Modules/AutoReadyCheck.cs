ad16b61fd1f5b9cdda7b5f9e3ce2ae16ba1cbeee
Christoph Müller <cmller49@gmail.com>
2019-09-02 19:59:05 +0200

    AutoReadyCheck should work

--- Library/Modules/AutoReadyCheck.cs
+++ Library/Modules/AutoReadyCheck.cs
@@ -6,10 +6,14 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 
+using Newtonsoft.Json;
+
 namespace Library.Modules
 {
-    public class AutoReadyCheck: BaseModule
+    public class AutoReadyCheck : BaseModule
     {
+        public const bool autoAccept = true; // TODO: Settings
+
         protected async override void OnEnable()
         {
             base.OnEnable();
@@ -22,17 +26,27 @@ namespace Library.Modules
 
         private async void OnReadyCheck(object sender, LeagueEvent e)
         {
-            Console.WriteLine("--- Response <GameFound> ---");
-            Console.WriteLine(e.Data);
-            Console.WriteLine("----------------------------");
+            ReadyCheckResponse res = e.Data.ToObject<ReadyCheckResponse>();
 
-            var api = await LeagueClientApi.ConnectAsync();
-            var queryParameters = Enumerable.Empty<string>();
-            var json = await api.RequestHandler.GetJsonResponseAsync(HttpMethod.Post, "/lol-matchmaking/v1/ready-check/accept", queryParameters);
+            if (autoAccept && res.playerResponse == "None")
+            {
+                Console.WriteLine("Game found!");
 
-            Console.WriteLine("--- Response <Accept> ---");
-            Console.WriteLine(json);
-            Console.WriteLine("-------------------------");
+                var api = await LeagueClientApi.ConnectAsync();
+                await api.RequestHandler.GetJsonResponseAsync(HttpMethod.Post, "/lol-matchmaking/v1/ready-check/accept");
+
+                Console.WriteLine("Accepted Readycheck!");
+            }
         }
+    }
+
+    struct ReadyCheckResponse           // TODO: Enums
+    {
+        public int[] declinerIds;
+        public string dodgeWarning;     // None, Warning, Penalty
+        public string playerResponse;   // None, Accepted, Declined
+        public string state;            // Invalid, InProgress, EveryoneReady, StrangerNotReady, PartyNotReady, Error
+        public bool suppressUx;
+        public double timer;
     }
 }
